540cc0d41acc09621d6dcf97f99ccf98a9c556f5
iMGamal <mohamed.gamal126027@gmail.com>
2023-10-01 02:39:12 +0000

    slacking

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * Print puts string
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s);
+{
+	//base case
+	if (puts == 0)
+		return 1;
+	else
+		return puts = puts * (puts - 1);
+
+	int main ()
+{
+	printf ("%d\n", puts(10));
+	return 0;
+}
