078636929f2a0b6ac3bf00c8717378552b00c531
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-07 16:36:59 +0000

    lulu

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,16 +1,6 @@
-#include<stdio.h>
-
 #ifndef _FUNCTION_LIKE_MACRO_H_
 #define _FUNCTION_LIKE_MACRO_H_
 
-#define ABS(x) 5
+#define ABS(x) (x =< 5)
 
 #endif
-
-int main()
-{
-	int x;
-	x = (1, 3, 5);
-	printf("%d\n", ABS(x));
-	return (0);
-}
