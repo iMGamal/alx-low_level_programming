41a097e8dad5727d75a1ff420022dd43bd1e2250
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-07 15:22:56 +0000

    is it fun?

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define _FILE_
+
+/**
+ * define - file
+ *
+ * Return: Always(0).
+ */
+
+int main()
+{
+	printf("File: %s\n)", __FILE__);
+
+	return (0);
+}
+
