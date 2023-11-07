54846e366d5d270fb55dc388a7c74294d0385935
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-07 14:17:13 +0000

    meh

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,13 +1,6 @@
-#include<stdio.h>
 #ifndef _OBJECT_LIKE_MACRO_H
 #define _OBJECT_LIKE_MACRO_H
-#define SIZE 1024
-#endif
 
-int main()
-{	
-	int n;
-	n = 76 + SIZE;
-	printf("%d\n", n);
-	return (0);
-}
+#define SIZE 1024
+
+#endif
