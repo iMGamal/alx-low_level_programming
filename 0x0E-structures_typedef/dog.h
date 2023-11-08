e6bb94e8ee924478f4fd15e5396edb82dfd25f15
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-08 00:26:45 +0000

    test2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * main - define new type struct dog
+ * name, type = char *
+ * age, type = float
+ * owner, type = char *
+ * Return: Always (0).
+ */
+
 struct dog
 {
 	char *name;
