9055e5ef8a23631f6c1aea6630c3aaf2cf36ca60
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-08 00:13:53 +0000

    test

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+struct dog my_dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+int main(void)
+{
+	typedef struct dog my_dog
+		my_dog.name ="Max";
+		my_dog.age =2;
+		my_dog.owner ="Zen";
+	return (0);
+}
