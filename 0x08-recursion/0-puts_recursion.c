c8618bd762974500c5675fc6389abd1400a1efd2
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-08 08:51:08 +0000

    com

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<main.h>
 
 /**
  * _puts_recursion - print a string
@@ -7,17 +6,16 @@
  * Return: Always (0).
  */
 
-void _puts_recursion(char *s);
-
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_puts(*s);
-		_puts_recursion(s + 1);
+		_puts_recursion(*s + 1);
 	}
 	else
 	{
-		_puts(\n);
+		_puts("\n");
 	}
 	return (0);
 }
