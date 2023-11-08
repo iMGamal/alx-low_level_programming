476ae976c3f23666634ad0e2fd04a31bf105f379
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-08 00:45:40 +0000

    roll

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,24 +6,16 @@
  * name, type = char *
  * age, type = float
  * owner, type = char *
+ * description - good dog
  * Return: Always (0).
  */
 
-#endif
 
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-int main(void)
-{
-	struct dog my_dog;
-	my_dog.name ="Zack";
-	my_dog.age ="3";
-	my_dog.owner ="Mace";
-
-	return (0);
 }
+
+#endif
