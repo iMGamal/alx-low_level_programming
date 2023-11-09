3ee8fc2c5f7a6bf080ea3f36c1e2266f26ea5869
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-09 03:58:29 +0000

    push

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include"function_pointers.h"
+
+/**
+ *int_index - indext of integer
+ *@array: array of functions
+ *@size: size of array
+ *@cmp: pointer to function
+ *
+ *Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int));
+{
+	int i = 0;
+
+	if(array && size && cmp)
+		while(i < size)
+		{
+			if(cmp(array[i]))
+				return (i);
+			i++;
+		}
+	return(-1);
+}
