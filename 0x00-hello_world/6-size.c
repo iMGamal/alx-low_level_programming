02462a11de958f96c6d4c25c9faa8ad81717c79f
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-10 07:45:18 +0000

    betty coding

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	printf("print size of char, %c\n",sizeof(char));
-	printf("print size of short, %hd\n",sizeof(short));
-	printf("print size of float, %f\n",sizeof(float));
-	printf("print size of double, %lf\n",sizeof(double));
+	printf("print size of char,%c\n", sizeof(char));
+	printf("print size of short,%hd\n", sizeof(short));
+	printf("print size of float,%f\n", sizeof(float));
+	printf("print size of double,%lf\n", sizeof(double));
 	return (0);
 }
