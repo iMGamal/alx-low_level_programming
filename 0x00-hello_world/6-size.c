256ccb4e7477a67089dae35f17f9246779959f92
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-10 07:38:21 +0000

    betty gcc warning

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
 /**
- * sizeof - a program that prints size of types
+ * main - a program that prints size of types
  * @char: data type with 1 byte size
  * @short: data type with 2 bytes size
  * @float: data type with 4 bytes size
  * @double: data type with 8 bytes size
  * Return: (0)
  */
-int sizeof(void,char,short,float,double)
+int main(void)
 {
 	printf("print size of char,%c\n",sizeof(char));
 	printf("print size of short,%hd\n",sizeof(short));
