635456e972b88528c8083dff0c2649fe4afc74d3
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-10 07:43:00 +0000

    betty coding document

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,13 @@
 
 /**
  * main - a program that prints size of types
- * @char: data type with 1 byte size
- * @short: data type with 2 bytes size
- * @float: data type with 4 bytes size
- * @double: data type with 8 bytes size
  * Return: (0)
  */
 int main(void)
 {
-	printf("print size of char,%c\n",sizeof(char));
-	printf("print size of short,%hd\n",sizeof(short));
-	printf("print size of float,%f\n",sizeof(float));
-	printf("print size of double,%lf\n",sizeof(double));
+	printf("print size of char, %c\n",sizeof(char));
+	printf("print size of short, %hd\n",sizeof(short));
+	printf("print size of float, %f\n",sizeof(float));
+	printf("print size of double, %lf\n",sizeof(double));
 	return (0);
 }
