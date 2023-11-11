443e95b4a36db09ec5235ea5245abb267ecfd9cc
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-11 09:14:07 +0000

    -13

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * _strlen_recursion - length of a string
+ * @s : string
+ * Return : Always 0 (success)
+ */
+int _strlen_recursion(char *s);
+{
+	int *s = 0;
+	if (*s)
+	{
+		_strlen_recursion(s + 1);
+	}
+}
