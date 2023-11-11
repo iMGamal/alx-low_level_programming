3fdc86e3fd5a46cd56f5409aae2fb4c6f15128b1
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-11 07:20:34 +0000

    string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,17 +3,18 @@
 
 /**
  * _puts_recursion - prints a string
- * @*s - pointer to string
+ *
+ * @s - pointer to string
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0');
 	{
-		_putchar("\n");
+	puts("*s");
+	_puts_recursion(s +1);
 	}
-	else
+	if (*s == '\0');
 	{
-		_putchar("*s");
-		_puts_recursion(s + 1);
+		puts("\n");
 	}
 }
