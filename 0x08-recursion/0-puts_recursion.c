0d76dcd4d81700eb8e31a67ccd020c3e7bcf8ebc
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-11 08:44:08 +0000

    6

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,12 @@
 /**
  * _puts_recursion - function like puts();
  * @s: input
- * Return: Always 0 (Success)
  **/
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		puts('\n');
+		puts("\n");
 	}
 	else
 	{
