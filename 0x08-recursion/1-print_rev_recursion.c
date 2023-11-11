1873bbd94a0209cb6464a114478af2be6c43f93d
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-11 08:49:31 +0000

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,18 +3,17 @@
 
 /**
  * _print_rev_recursion - printing in reverse
- * @char - data type
- * @*s - variable
+ * @s - variable
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0');
+	if (*s == '\0')
 	{
-		_putchar(*s);
+		printf("*s");
 	}
 	else
 	{
 		_print_rev_recursion (s +1);
-		_putchar(*s)
+		printf("*s")
 	}
 }
