bca6f3ff3d4cb4291a3b8633766af2d9b5c606b5
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-11 07:22:54 +0000

    if

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0');
+	if (*s != '\0')
 	{
 	puts("*s");
 	_puts_recursion(s +1);
 	}
-	if (*s == '\0');
+	if (*s == '\0')
 	{
 		puts("\n");
 	}
