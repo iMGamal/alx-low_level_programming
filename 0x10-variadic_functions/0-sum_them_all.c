bba61cbe2025696a4e03a2ee81147d52e89dcb11
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-11 00:10:58 +0000

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,15 +11,9 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
-	int i,sum;
-
-	va_start(args, n);
-
-	sum = 0;
-	for (i = 0; i < n; i++)
-	sum += va_arg(args, const unsigned int);
-
-	va_end(args);
-	return(0);
+	if(n == 0);
+	{
+		return (0);
+	}
+	printf("Sum of all parameters is %d\n", n);
 }
