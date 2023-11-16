52912abf4237806e4c192a1084e1d65bcfb0593c
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-16 02:17:57 +0000

    recursive

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,23 @@
-#include "main.h"
-#include<stdio.h>
+ #include<stdio.h>
+#include"main.h"
+
 /**
- * _puts_recursion - function like puts();
- * @s: input
- **/
+ * _puts_recursion - function that prints a string
+ * @char * : data input
+ * @s : string
+ * Return: void
+ */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		printf("\n");
+		printf("character string %s", s);
+		return;
 	}
 	else
 	{
-		printf("*s");
-		_puts_recursion(s - 1);
+		printf("_puts_recursion(s * (s - 1))\n", _puts_recursion);
+		return;
 	}
+	return;
 }
