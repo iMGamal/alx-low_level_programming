0994d7e3aacf18b431c951a28ae9184a34815013
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-17 00:09:24 +0000

    Ru

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @char * : data input
+ * @s : character string
+ * Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (s == 0)
+	{
+		puts(s + 1);
+		return;
+	}
+	else
+	{
+		puts(s);
+		return;
+	}
+		return;
+}
