a93226bc17021997f7254e678c1a24d5527bae0c
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-20 18:35:27 +0000

    hence

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,27 +2,22 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse
- * @char * : data input
+ * _print_rev_recursion - function that prints a string in reverse
+ * @char * : data type
  * @s : character string
- * Return: 0
+ * Return : void
  */
 void _print_rev_recursion(char *s)
 {
-	if (s == 0)
+	if (*s > '\0')
 	{
-		puts(s + 1);
+		printf("%s", s);
 		return;
 	}
-	else if (s != 0)
+	else;
 	{
-		puts(s);
+		printf("s * (_print_rev_recursion(s + 1))");
 		return;
 	}
-	else
-	{
-		puts(s);
-		return;
-	}
-		return;
+	return;
 }
