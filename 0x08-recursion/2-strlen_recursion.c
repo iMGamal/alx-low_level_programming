80074e781f61f34cfa0a73ef0099dc2c5aa76cc0
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-21 06:32:34 +0000

    len

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,13 +11,14 @@ int _strlen_recursion(char *s)
 {
 	int length = 0;
 
-	if (*s > 0)
+	if (*s == 0)
 	{
 		printf("%s", s);
 	}
-	else if (*s == 0)
+	else if (*s > 0)
 	{
-		length += (_strlen_recursion(s + 1) + 1);
+		length = _strlen_recursion(s + 1);
+		printf("%d", length);
 	}
 	return (length);
 }
