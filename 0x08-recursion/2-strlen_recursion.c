24fed44f67bb4b6b32fc57f4f17ac5388dce8750
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-21 06:59:39 +0000

    hazing

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,7 +5,7 @@
  * _strlen_recursion - length of string
  * @char * : data type
  * @s : character string
- * Return : _strlen_recursion(s + 1) + 1
+ * Return : 
  */
 int _strlen_recursion(char *s)
 {
@@ -13,7 +13,8 @@ int _strlen_recursion(char *s)
 
 	if (*s > '\0')
 	{
-		length += _strlen_recursion(s + 1) + 1;
+		length ++;
+		length += _strlen_recursion(s + 1);
 	}
 	return (length);
 }
