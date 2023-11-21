7d5d9e41dd842fd35d77ef4e90d502bdb38e3c32
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-21 01:21:48 +0000

    string length 2.0

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,15 +5,21 @@
  * _strlen_recursion - length of string
  * @char * : data type
  * @s : character string
- * Return : integer value of string length
+ * Return : 0
  */
 int _strlen_recursion(char *s)
 {
 	int strlen = 0;
 
-	if(*s == '\0')
+	if (*s == '\0')
 	{
 		printf("%d", strlen);
+		return (0);
+	}
+	else
+	{
+		_strlen_recursion(s + 1);
+		putchar(*s);
 		return (0);
 	}
 	return (0);
