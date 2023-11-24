a584da2cebac74c0a9b2f5dbe74f1369ad7b6fbe
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-24 01:20:19 +0000

    5

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,7 @@
 /**
  * mallo_checked - function returns nothing
  * @b : parameter whose soze to be checked
- * Return : void
+ * Return : pointer to allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
