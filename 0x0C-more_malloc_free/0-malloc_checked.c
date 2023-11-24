4f7066efb56334b171887dc0d08ac56f330f825a
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-24 00:45:12 +0000

    1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * mallo_checked - function returns nothing
+ * @b : parameter whose soze to be checked
+ * Return : void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *n;
+
+	n = malloc(b);
+	if (b == 0)
+	{
+		exit(98);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
