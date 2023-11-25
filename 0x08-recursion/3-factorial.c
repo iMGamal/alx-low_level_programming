f928384b799d82cb2fd7a8e8067dff3a7bcf894b
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-25 10:49:05 +0000

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,19 +3,18 @@
 
 /**
  * factorial - function that returns factorial of a given number
- * @n - variable
- * Return : positive number
+ * @n - integer number whose factorial value to be returned
+ * Return : factorial of n
  */
 int factorial(int n)
 {
 	if (n < 0)
 	{
-		return -1;
+		return (-1);
 	}
-	if (n != 0)
+	if ((n = 0) || (n > 0))
 	{
-		return factorial(n);
+		return (factorial(n));
 	}
-	putchar(factorial(n + 1));
-	return n;
+	return (factorial(n));
 }
