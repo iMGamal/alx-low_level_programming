48b5f5ef5b3f1b64706fef6d84498d7f77430685
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-26 08:21:31 +0000

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_palindrome - function
+ * @s : empty string
+ * @return : palindrome
+ */
+int is_palindrome(char *s)
+{
+	if (*s < 0 || *s == 0)
+	{
+		return (0);
+	}
+	else if (is_palindrome(s - 1) == is_palindrome(s))
+	{
+		return (1);
+	}
+	return (*s);
+}
