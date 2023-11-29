6ed699e43b1eb1eae18fe6f4f0f26ddf46107a6a
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-29 02:20:12 +0000

    1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
-#include "string.h"
+#include <stdlib.h>
 
 /**
  * init_dog - function that returns nothing
@@ -12,9 +12,13 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	strcpy(d->name, "Max");
-	d->age = 2.5;
-	strcpy(d->owner, "Erik");
-	printf("Dog name is %s, its age is %f and its owner called %s", d->name, d->age, d->owner);
+	if (d == NULL)
+	{
+		d = maloc(sizeof(struct dog));
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+		return;
+	}
 	return;
 }
