e2a77d2915d25d25788b8544a49270ce57c6f6eb
iMGamal <mohamed.gamal126027@gmail.com>
2023-12-24 07:32:24 +0000

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include "stdio.h"
 /**
  * main : function that prints
- * return : always 0
+ * Return : Always 0 (Success)
  */
-
 int main()
 {
 	puts("\"Programming is like building a multilingual puzzle");
