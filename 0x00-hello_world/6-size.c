b2417c3401d51afa74af458ee869fbd69e69b002
iMGamal <mohamed.gamal126027@gmail.com>
2023-12-24 08:39:40 +0000

    si

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	printf("sizeof(char)\n");
-	printf("sizeof(int)\n");
-	printf("sizeof(long int)\n");
-	printf("sizeof(long long)\n");
-	printf("sizeof(float)\n");
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
 	return (0);
 }
 
