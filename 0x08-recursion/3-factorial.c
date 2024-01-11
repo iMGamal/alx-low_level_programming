db72092cc38a4a744ebfc38c76caf8b14121a08d
iMGamal <mohamed.gamal126027@gmail.com>
2024-01-11 01:01:26 +0000

    4th yes

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * factorial - Entry point
+ * Description - function that returns factorial of a given number
+ * @n: integer
+ * Return - Always 0 (Success)
+ */
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		putchar(n + 1);
+		return (0);
+	}
+}
