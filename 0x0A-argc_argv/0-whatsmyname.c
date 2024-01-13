9d939fcdd05eb6d90ec521767273e72da937561c
iMGamal <mohamed.gamal126027@gmail.com>
2024-01-13 00:38:12 +0000

    test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: a program that print its name
+ * @argc: number of arguments supplied to main
+ * @argv[]: array of pointers to strings
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0 && argv[argc] == NULL)
+	{
+		printf("%d\n",argc);
+		return (0);
+	}
+	return (0);
+}
