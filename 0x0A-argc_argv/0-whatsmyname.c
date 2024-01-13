d0f305459e35359adddc520d51d7f8bebdfc6f64
iMGamal <mohamed.gamal126027@gmail.com>
2024-01-13 01:25:16 +0000

    int main()

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,8 +3,9 @@
  * main - Entry point
  * argc - number of arguments supplied to main
  * argv[] - array of pointers to strings
+ * Return - Always 0 (Success)
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i = 0;
 
@@ -12,6 +13,7 @@ void main(int argc, char *argv[])
 	{
 		printf("%s\n", argv[i]);
 		i++;
+		return (0);
 	}
-	return;
+	return (0);
 }
