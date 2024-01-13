cbd57e8e5c0759baa377a72b1f2099260eed8d6b
iMGamal <mohamed.gamal126027@gmail.com>
2024-01-13 02:22:56 +0000

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * argc - Number of arguments
+ * argv - array of pointers to strings
+ * Return - Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+
+	int outcome = 1;
+
+	if (argc > i && argv[argc] == NULL)
+	{
+		outcome == outcome * argv[i];
+		printf("%d\n", outcome);
+		i++
+		return (0);
+	}
+	return (0);
+}
