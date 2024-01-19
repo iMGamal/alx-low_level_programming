fcd04a2383ebf627c14efa6345f31a6b47ee6148
iMGamal <mohamed.gamal126027@gmail.com>
2024-01-19 22:27:54 +0000

    -1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,8 @@
  * argv[] - array of pointers to strings
  * Return: Always 0 (Success)
  */
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, __attribute__((unused))char *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
