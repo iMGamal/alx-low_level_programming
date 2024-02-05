e50bcecb845571811bde78146881b01258fe636c
iMGamal <mohamed.gamal126027@gmail.com>
2024-02-05 10:41:43 +0000

    32

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,19 +4,17 @@
  * factorial - Entry point
  * Description - function that returns factorial of a given number
  * @n: integer
- * Return: x
+ * Return: n * (factorial( n - 1)
  */
 int factorial(int n)
 {
-	int x = (n * factorial(n - 1));
-
 	if (n < 0)
 	{
 		return (-1);
 	}
-	else
+	if (n == 0)
 	{
-		return (n * factorial(n - 1));
+		return (1);
 	}
-	return (x);
+	return (n * (factorial(n - 1)));
 }
