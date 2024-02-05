7d205d02de2cde9be887dee1bb4509a984b2846e
iMGamal <mohamed.gamal126027@gmail.com>
2024-02-05 09:13:12 +0000

    20

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,13 +4,13 @@
  * _strlen_recursion - Entry points
  * Description - function that returns length of string
  * @s : character string
- * Return : *s
+ * Return : char
  */
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
+		_strlen_recursion(s - 1);
 	}
 	return (*s);
 }
