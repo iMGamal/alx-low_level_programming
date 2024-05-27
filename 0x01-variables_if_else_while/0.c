bf3de00a6a2bbb7a6ae82eb870b0be1942249021
iMGamal <mohamed.gamal126027@gmail.com>
2024-05-27 13:35:31 +0000

    eq

---
+++ 0x01-variables_if_else_while/0.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	char *n;
+
+	n = "abcdfghijklmnoprstuvwxyz";
+	putchar(*n);
+	putchar(10);
+	return 0;
+}
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-/**
- * main - Prints the alphabet without q and e.
- *
- * Return: Always 0 (Success)
+/*
+ * main - Entry point
+ * Description: program that prints alphabet except for e and q
+ * Return: Always 0 (success)
  */
-int main(void)
+int main()
 {
 	int i;
 
 	for (i = 97; i < 123; i++)
 	{
-	if (i != 101 && i != 113)
+		if (i != 101 && i != 113)
 		{
 			putchar(i);
 		}
 	}
-	putchar('\n');
-	return (0);
+	putchar(10);
+	return 0;
 }
