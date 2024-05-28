fc9cf50f84766057893aec4a75105ed30e785933
iMGamal <mohamed.gamal126027@gmail.com>
2024-05-28 03:14:34 +0000

    II

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (i = 48; i <= 57; i++)
 	{
-		for (j = 49; j <= 57; j++)
+		for (j = i + 1; j <= 57; j++)
 		{
-			if (i < j)
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
 			{
-				putchar(i);
-				putchar(j);
 				putchar(',');
 				putchar(' ');
 			}
