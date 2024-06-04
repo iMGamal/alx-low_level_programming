cfa30659f20ede11f18c2c0bad1f567baaa81c40
iMGamal <mohamed.gamal126027@gmail.com>
2024-06-04 23:45:49 +0000

    89

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for island_perimeter function."""
+
+
+def island_perimeter(grid):
+    """RETURNS the perimeter of the island."""
+    rows = len(grid)
+    columns = len(grid[0])
+    perimeter = 0
+
+    for n in range(rows):
+        for s in range(columns):
+            if grid [n][s] == 1:
+                perimeter += 4
+            if n > 0 and grid [n - 1][s] == 1:
+                perimeter -= 2
+            if s > 0 and grid [n][s - 1] == 1:
+                perimeter -= 2
+
+    return perimeter
