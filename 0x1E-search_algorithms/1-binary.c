645e13bb01244354d1d525cb6e85a94b4fc1a827
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-17 22:26:41 +0000

    task 1 v5

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,11 +11,10 @@
 int binary_search(int *array, size_t size, int value)
 {
 	int low, mid, high, i;
-
 	low = 0;
 	high = size - 1;
 
-	if (array == NULL)
+	if (!value || array == NULL)
 	{
 		return (-1);
 	}
