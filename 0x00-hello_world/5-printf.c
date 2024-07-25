d86008e48fd8584cedf861705b4cb56a1a0d90d2
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-25 14:07:56 -0700

    First C program with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints a statement
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
