1ac38b5d47be586c632f6c93b1cdff322af830f2
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-25 21:38:49 -0700

    Slowly but surely

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints letters
+ * Return: 0;
+ */
+int main(void)
+{
+	int n, x;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n < 57)
+		{
+			putchar(',');
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}
