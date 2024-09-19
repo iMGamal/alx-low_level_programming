06925045684a17268a1d32bb3616887d33358575
iMGamal <mohamed.gamal126027@gmail.com>
2024-09-19 21:41:05 +0000

    Task 4 v4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Function that prints a sentence
+ * Return: 0
+ */
 int main()
 {
 	puts("\"Programming is like building a multilingual puzzle");
