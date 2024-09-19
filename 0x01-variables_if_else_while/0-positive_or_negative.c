4593551be94cf095d2f1486e849a850cbd0010b0
iMGamal <mohamed.gamal126027@gmail.com>
2024-09-19 23:39:56 +0000

    Task 0 v3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there *
- *
- * betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: Function that prints an integer and checks its sign
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
